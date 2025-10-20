2e30f6a10277892d1b7be2774fb184e67127f066
RavySaksena <ravysaksena5@gmail.com>
2025-10-20 11:29:29 +0700

    buat page lagi

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>About TechNova Solutions</h1>
+      <p>Founded in 2024, TechNova Solutions has been a pioneer in providing bespoke software development, cloud computing, and digital marketing services. Our mission is to empower businesses with technology.</p>
+    </div>
+  );
+}
+
+export default About;
